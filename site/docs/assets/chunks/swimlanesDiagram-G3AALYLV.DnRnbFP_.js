bca34dc0684e76e898ef1df34200322be1eba5d8
Laio O. Seman <laioseman@gmail.com>
2026-07-29 17:40:09 -0300

    style: polish landing page footer, section headers, and path cards

---
+++ site/docs/assets/chunks/swimlanesDiagram-G3AALYLV.DnRnbFP_.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U.wC3tZl5x.js";import{_ as a}from"./theme.CZTW8ipR.js";import"./chunk-5VM5RSS4.9Srd6NyJ.js";import"./chunk-XXDRQBXY.CUpAxsIG.js";import"./chunk-VR4S4FIN.BBytHRrg.js";import"./chunk-32BRIVSS.CruNOSbe.js";import"./framework.C_G3Qjer.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
